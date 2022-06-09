6aa172ff99fe59312f5372356544fd1a59fa0b08
Fraser Watt <fsrwatt@gmail.com>
2022-06-09 13:12:27 +0100

    making Upvote poster_id optional for demonstrative purposes

--- CommentsProject/CommentsProject/settings.py
+++ CommentsProject/CommentsProject/settings.py
@@ -134,3 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"*localhost*",
+]
