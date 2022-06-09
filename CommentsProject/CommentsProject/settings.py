921f3f41cc270a58673f427ac828039c6784665b
Fraser Watt <fsrwatt@gmail.com>
2022-06-09 13:14:31 +0100

    Allowing localhost

--- CommentsProject/CommentsProject/settings.py
+++ CommentsProject/CommentsProject/settings.py
@@ -133,7 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# CORS_ALLOWED_ORIGINS = ["*"]
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"*localhost*",
-]
+CORS_ALLOWED_ORIGINS = ["http://localhost:58856/"]
