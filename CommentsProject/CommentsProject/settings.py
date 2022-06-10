18388f6a74518f6c92a79854cb3db4a2cb154481
Fraser Watt <fsrwatt@gmail.com>
2022-06-10 11:34:15 +0100

    changing localhost for testing

--- CommentsProject/CommentsProject/settings.py
+++ CommentsProject/CommentsProject/settings.py
@@ -133,4 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:58856/"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000/"]
